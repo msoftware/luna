0a42e9e4f4e3dd3a7f24b1d972dcdf0d1d560e86
Kate Temkin <k@ktemkin.com>
2020-03-09 18:55:21 -0600

    apollo: add support for external boards; and run on Daisho

--- luna/apollo/onboard_jtag.py
+++ luna/apollo/onboard_jtag.py
@@ -6,11 +6,23 @@
 from .jtag import JTAGDevice
 
 
+#
+# LUNA boards.
+#
+
 class LatticeECP5_12F(JTAGDevice):
     """ Class representing a JTAG-connected ECP5. """
 
     DESCRIPTION = "Lattice LFE5U-12F ECP5 FPGA"
-
-    # A list of supported IDCODEs for the relevant class.
-    # Used unless the supports_idcode() method is overridden.
     SUPPORTED_IDCODES = [0x21111043]
+
+
+#
+# Daisho boards.
+#
+
+class IntelCycloneIV_EP4CE30(JTAGDevice):
+    """ Class representing a JTAG-connected CycloneIV, as on Daisho. """
+
+    DESCRIPTION = "Intel/Altera EP4CE30 Cyclone-IV FPGA"
+    SUPPORTED_IDCODES = [0x020f40dd]
